5d09d7670dfda194d75e467ef7dde3640da09848
samuel <samuelmk2@126.com>
2013-01-18 17:12:11 +0800

    2013-1-18 exercise

--- c++/c++ primer exercise/9_section/9_section.cpp
+++ c++/c++ primer exercise/9_section/9_section.cpp
@@ -7,7 +7,8 @@ using namespace std;
 //#define EXERCISE_9_9
 //#define EXERCISE_9_12
 //#define EXERCISE_9_13
-#define EXERCISE_9_14
+//#define EXERCISE_9_14
+#define EXERCISE_9_26
 
 int a[10] = {1,2,3,4,5,6,7,8,9,10};
 
@@ -30,7 +31,7 @@ void exercise_9_9(void)
 
 
 /* Exercise 9.12
-** 
+**
 */
 bool exercise_9_12(vector<int>::iterator first,vector<int>::iterator last,int n)
 {
@@ -45,7 +46,7 @@ bool exercise_9_12(vector<int>::iterator first,vector<int>::iterator last,int n)
 }
 
 /* Exercise 9.13
-** 
+**
 */
 vector<int>::iterator exercise_9_13(vector<int>::iterator first,vector<int>::iterator last,int n)
 {
@@ -63,33 +64,95 @@ vector<int>::iterator exercise_9_13(vector<int>::iterator first,vector<int>::ite
 
 
 /* Exercise 9.14
-** 
+** 使用迭代器编写程序：从标准输入设备读入若干string对象，并将它们存入一个vector对象中，然后输入该vector对象中的所有元素。
 */
 void exercise_9_14()
 {
+	cout << "here is exercise 9.14:" << endl;
 
+	vector<string> svec;
+	string str;
+
+	cout << "Input some strings(Ctrl+z to end) " << endl;
+	while (cin >> str)
+		svec.push_back(str); // 把string加入到vector中
+		
+	vector<string>::iterator iterFirst = svec.begin();
+	vector<string>::iterator iterLast = svec.end();
+	
+	while (iterFirst != iterLast)
+		cout << *(iterFirst++) << endl;
 }
 
+
+/* Exercise 9.26
+** 假设有如下 ia 的定义，将 ia 复制到一个 vector 容器和一个 list 容器中。使用单个迭代器参数版本的
+** erase 函数将 list 容器中的奇数值元素删除掉，然后将 vector 容器中的偶数值元素删除掉。
+** int ia[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89 };
+*/
+void exercise_9_26()
+{
+	cout << "here is exercise 9.14:" << endl;
+	
+	int ia[] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89 };
+	
+	vector<int> ivec(ia,ia + 11); // a+11,指到最后一个元素的下一位。
+	list<int> ilist(ia,ia + 11);
+	
+	// 删除vector中的偶数值
+	vector<int>::iterator iIter = ivec.begin();
+	while (iIter != ivec.end())
+	{
+		if ((*iIter % 2) == 0)
+			iIter = ivec.erase(iIter); // 因为erase方法返回指向被删元素的下一个元素，所以再次赋给iIter即可，不用再做iIter++的动作
+		else
+			iIter++;
+	}
+	
+	// 重新取得新的迭代器，打印验证
+	vector<int>::iterator iIter2 = ivec.begin();
+	cout << "我们是奇数!" << endl; 
+	while (iIter2 != ivec.end())
+		cout << *(iIter2++) << endl;
+		
+	/////////////////////////////////////////////////////////////////////////////////	
+		
+	// 删除list中的奇数值
+	list<int>::iterator iIter3 = ilist.begin();
+	while (iIter3 != ilist.end())
+	{
+		if ((*iIter3 % 2) != 0)
+			iIter3 = ilist.erase(iIter3); // 因为erase方法返回指向被删元素的下一个元素，所以再次赋给iIter即可，不用再做iIter++的动作
+		else
+			iIter3++;
+	}
+	
+	// 重新取得新的迭代器，打印验证
+	list<int>::iterator iIter4 = ilist.begin();
+	cout << "而我们是偶数!" << endl; 
+	while (iIter4 != ilist.end())
+		cout << *(iIter4++) << endl;
+}
 
 int main()
 {
 	cout << "prepare to exercise." << endl;
-	
+
 	#ifdef EXERCISE_9_9
 		exercise_9_9();
-	
+
 	#elif defined EXERCISE_9_12
-		vector<int> ivector(a,a + 10); 
+		vector<int> ivector(a,a + 10);
 		vector<int>::iterator iter1 = ivector.begin();
 		vector<int>::iterator iter2 = ivector.end();
 		int temp = exercise_9_12(iter1,iter2,9);
 		cout << "temp = " << temp << endl;
-		
+
 	#elif defined EXERCISE_9_13
 		cout << "Please input the number you want to find" << endl;
 		int input;
 		cin >> input;
-		vector<int> ivector(a,a + 10); 
+		vector<int> ivector(a,a + 10);
 		vector<int>::iterator iter1 = ivector.begin();
 		vector<int>::iterator iter2 = ivector.end();
 		vector<int>::iterator rIter = exercise_9_13(iter1,iter2,input);
@@ -97,11 +160,15 @@ int main()
 			cout << "sorry ,you can't find the number" << endl;
 		else
 			cout << "congratulations ,you find the number!" << endl;
-	
+
 	#elif defined EXERCISE_9_14
-	
+		exercise_9_14();
+		
+	#elif defined EXERCISE_9_26
+		exercise_9_26();
+		
 	#else
 	#endif
-	
+
 	return 1;
 }
