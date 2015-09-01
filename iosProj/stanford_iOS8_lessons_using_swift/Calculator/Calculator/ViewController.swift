4d2bbc78cf90decbfaeb1d374e59aa7175a47adc
lemon_mac <link1226@126.com>
2015-09-02 00:25:16 +0800

    update

--- iosProj/stanford_iOS8_lessons_using_swift/Calculator/Calculator/ViewController.swift
+++ iosProj/stanford_iOS8_lessons_using_swift/Calculator/Calculator/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle
+        println("digit is \(digit)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
