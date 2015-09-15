06368d0a47b0401fe02d50329c9284479e28980a
lemon_mac <link1226@126.com>
2015-09-15 22:26:01 +0800

    add gameDemo folder

---
+++ iosProj/GameDemo/ZombieConga/ZombieConga/GameViewController.swift
@@ -0,0 +1,85 @@
+//
+//  GameViewController.swift
+//  ZombieConga
+//
+//  Created by lemon_mac on 15-6-22.
+//  Copyright (c) 2015年 com.lemon. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
+    }
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
+//extension SKNode {
+//    class func unarchiveFromFile(file : NSString) -> SKNode? {
+//        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+//            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+//            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+//            
+//            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+//            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+//            archiver.finishDecoding()
+//            return scene
+//        } else {
+//            return nil
+//        }
+//    }
+//}
+//
+//class GameViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+//            // Configure the view.
+//            let skView = self.view as SKView
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
+//            
+//            /* Sprite Kit applies additional optimizations to improve rendering performance */
+//            skView.ignoresSiblingOrder = true
+//            
+//            /* Set the scale mode to scale to fit the window */
+//            scene.scaleMode = .AspectFill
+//            
+//            skView.presentScene(scene)
+//        }
+//    }
+//
+//    override func shouldAutorotate() -> Bool {
+//        return true
+//    }
+//
+//    override func supportedInterfaceOrientations() -> Int {
+//        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+//            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+//        } else {
+//            return Int(UIInterfaceOrientationMask.All.rawValue)
+//        }
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Release any cached data, images, etc that aren't in use.
+//    }
+//
+//    override func prefersStatusBarHidden() -> Bool {
+//        return true
+//    }
+//}
