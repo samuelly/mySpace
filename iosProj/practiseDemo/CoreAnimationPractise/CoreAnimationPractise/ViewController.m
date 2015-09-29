e74edcc7a3f815a377d330ee43a17a0574b0ffc2
lemon_mac <link1226@126.com>
2015-09-29 23:42:47 +0800

    update

--- iosProj/practiseDemo/CoreAnimationPractise/CoreAnimationPractise/ViewController.m
+++ iosProj/practiseDemo/CoreAnimationPractise/CoreAnimationPractise/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) UIView *testView;
 @end
 
 @implementation ViewController
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    [self initView];
+}
+
+- (void)initView
+{
+    _testView = [[UIView alloc] initWithFrame:CGRectMake((self.view.frame.size.width-200)/2, (self.view.frame.size.height-200)/2, 200, 200)];
+    _testView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_testView];
+    
+    CALayer *blueLayer = [CALayer layer];
+    blueLayer.frame = CGRectMake(50, 50, 100, 100);
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    [_testView.layer addSublayer:blueLayer];
+    
 }
 
 - (void)didReceiveMemoryWarning {
