4bd818396a8278d97b805363804334c067a777b5
zhuqi <zhuqi@superjia.com>
2017-01-03 19:47:17 +0800

    add proj:Your Air

---
+++ iosProj/Your Air/Your Air/YA files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Your Air
+//
+//  Created by zhuqi on 17/1/3.
+//  Copyright © 2017年 zhuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
